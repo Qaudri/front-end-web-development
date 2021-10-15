555f7812eef74a6d25b5066eda2b349cd0d4835a
Qaudri <aquadirmuhammad@gmail.com>
2021-10-15 16:35:50 +0100

    Added Javascript functionalities for more interactvity

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// alert("Hello World");
+
+var name = prompt("Please enter your name");
+alert("Welcome back " + name );
+
+// A simple calculator
+var first_value = prompt("Please enter first value");
+var second_value = prompt("Please enter second value");
+var result = first_value * second_value;
+
+alert("The product of " + first_value + " x " + second_value + " = " + result );
