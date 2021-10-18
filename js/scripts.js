b79d05928693c121a0babe17bf69885ae82381ce
Qaudri <aquadirmuhammad@gmail.com>
2021-10-18 16:38:43 +0100

    refactor: added a menu and close button for phone breakpiont

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,37 @@
 // alert("Hello World");
 
-var name = prompt("Please enter your name");
-alert("Welcome back " + name );
+// var name = prompt("Please enter your name");
+// alert("Welcome back " + name );
+
 
 // A simple calculator
-var first_value = prompt("Please enter first value");
-var second_value = prompt("Please enter second value");
-var result = first_value * second_value;
+// var first_value = prompt("Please enter first value");
+// var second_value = prompt("Please enter second value");
+// var result = first_value * second_value;
 
-alert("The product of " + first_value + " x " + second_value + " = " + result );
+// alert("The product of " + first_value + " x " + second_value + " = " + result );
+
+                                             
+//open menu container
+
+var menu_button = document.getElementById("menu_btn");
+var close_button = document.getElementById("close_btn");
+
+menu_button.addEventListener("click", open_menu);
+close_button.addEventListener("click", close_menu);
+
+function open_menu(){
+    // alert("I am working")
+    let menu_cont = document.getElementById("menu_container")
+    menu_cont.style.display = "block";
+    menu_button.style.display = "none";
+    close_button.style.display = "block";
+}
+
+function close_menu(){
+    // alert("I am working")
+    let menu_cont = document.getElementById("menu_container")
+    menu_cont.style.display = "none";
+    menu_button.style.display = "block";
+    close_button.style.display = "none";
+}
